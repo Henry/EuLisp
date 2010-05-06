dbaf2d67ae96433e0ea26e688a725f718d87194c
henry <Henry Weller h.weller@opencfd.co.uk>
2010-05-06 22:53:57 +0100

    Second step towards a string manipulation library based on POSIX and PCRE: match and match-all.

--- Examples/StringPerls/eul-string-perls.c
+++ Examples/StringPerls/eul-string-perls.c
@@ -5,13 +5,14 @@
 ///-----------------------------------------------------------------------------
 
 #include "eul-string-perls.h"
+#include "pcre.h"
 
 ///-----------------------------------------------------------------------------
 
 ///-----------------------------------------------------------------------------
 /// Split
 ///-----------------------------------------------------------------------------
-LispRef eul_split_string(char *str, char *delimiters)
+LispRef eul_split_string(const char *str, const char *delimiters)
 {
     #ifdef DEBUG
     printf
@@ -41,6 +42,137 @@ LispRef eul_split_string(char *str, char *delimiters)
         eul_allocate_cons(res, eul_substr, res);
 
         substr = strtok(NULL, delimiters);
+    }
+
+    return res;
+}
+
+///-----------------------------------------------------------------------------
+/// PCRE Regular Expression handling
+///-----------------------------------------------------------------------------
+
+// Compile and return the regular expression
+pcre *eul_compile_regex(const char *regex)
+{
+    const char *error;
+    int erroffset;
+
+    pcre *re = pcre_compile
+    (
+        regex,      // the pattern
+        0,          // default options
+        &error,     // for error message
+        &erroffset, // for error offset
+        NULL        // use default character table
+    );
+
+    if (!re)
+    {
+        #ifdef DEBUG
+        fprintf
+        (
+            stderr,
+            "PCRE compilation failed at expression offset %d: %s\n",
+            erroffset,
+            error
+        );
+        #endif
+    }
+
+    return re;
+}
+
+#define OVECCOUNT 30 // should be a multiple of 3
+
+// Match the next occurrence of the regular expression
+// and reset the start of the string to the end of the occurrence
+LispRef eul_match_next_string(const char **strPtr, const pcre* re)
+{
+    const char *str = *strPtr;
+    int ovector[OVECCOUNT];
+
+    int rc = pcre_exec
+    (
+        re,           // the compiled pattern
+        NULL,         // no extra data - we didn't study the pattern
+        str,          // the subject string
+        strlen(str),  // the length of the subject
+        0,            // start at offset 0 in the subject
+        0,            // default options
+        ovector,      // output vector for substring information
+        OVECCOUNT     // number of elements in the output vector
+    );
+
+    if (rc < 0)
+    {
+        switch(rc)
+        {
+            case PCRE_ERROR_NOMATCH:
+                #ifdef DEBUG
+                printf("No match found in text\n");
+                #endif
+                break;
+
+                // More cases defined...
+
+            default:
+                #ifdef DEBUG
+                printf("Match error %d\n", rc);
+                #endif
+                break;
+        }
+    }
+
+    LispRef res = eul_nil;
+
+    if (rc > 0)
+    {
+        // Loop over the matches in reverse-order
+        // to cons into the list in the correct order
+        for (int i=rc-1; i>=0; i--)
+        {
+            int j = 2*i;
+
+            LispRef eul_substr;
+            const char *start = str + ovector[j];
+            int length = ovector[j+1] - ovector[j];
+            eul_allocate_nstring(eul_substr, start, length);
+            eul_allocate_cons(res, eul_substr, res);
+        }
+
+        // Reset the start of the search string to the end of the last match
+        *strPtr = str + ovector[2*(rc - 1) + 1];
+    }
+
+    return res;
+}
+
+LispRef eul_match_string(const char *str, const char *regex)
+{
+    pcre* re = eul_compile_regex(regex);
+
+    if (re)
+    {
+        const char *strStart = str;
+        return eul_match_next_string(&strStart, re);
+    }
+    else
+    {
+        return eul_nil;
+    }
+}
+
+LispRef eul_match_all_string(const char *str, const char *regex)
+{
+    LispRef match, res = eul_nil;
+    pcre* re = eul_compile_regex(regex);
+
+    if (re)
+    {
+        while ((match = eul_match_next_string(&str, re)) != eul_nil)
+        {
+            eul_allocate_cons(res, match, res);
+        }
     }
 
     return res;
--- Examples/StringPerls/eul-string-perls.h
+++ Examples/StringPerls/eul-string-perls.h
@@ -14,7 +14,10 @@
 
 // Split the string str at the delimiters and return a list of the sub-strings
 // Note: the list returned is in reverse order
-extern LispRef eul_split_string(char *str, char *delimiters);
+extern LispRef eul_split_string(const char *str, const char *delimiters);
+
+extern LispRef eul_match_string(const char *str, const char *regex);
+extern LispRef eul_match_all_string(const char *str, const char *regex);
 
 ///-----------------------------------------------------------------------------
 #endif // EUL_STRING_PERLS_H
